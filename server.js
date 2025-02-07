da7df49412e4a534a3f363bea11fb75d759496c3
rafael valbuena <artillero.satanas@gmail.com>
2025-02-07 17:15:42 -0500

    agregadas funcionalidades para imagenes

--- server.js
+++ server.js
@@ -32,15 +32,51 @@ io.on('connection', (socket) => {
 
   // Escuchar y reenviar mensajes privados
   socket.on('privateMessage', ({ to, message, from }) => {
-    const recipientSocketId = Object.keys(users).find(key => users[key] === to);
+    const recipientSocketId = Object.keys(users).find((key) => users[key] === to);
     if (recipientSocketId) {
       io.to(recipientSocketId).emit('privateMessageResponse', { from, message });
     }
   });
 
-  //ventana privada de chat
-  socket.on("openPrivateChat", ({ recipient, sender }) => {
-    socket.to(recipient).emit("openPrivateChat", sender);
+  // Escuchar y reenviar archivos en el chat general
+  socket.on('fileUpload', (data) => {
+    io.emit('fileResponse', data); // Enviar archivo a todos
+  });
+
+  // Escuchar y reenviar archivos en chats privados
+  socket.on('privateFileUpload', ({ to, file, fileName, from }) => {
+   
+    // Enviar el archivo al destinatario
+    const recipientSocketId = Object.keys(users).find((key) => users[key] === to);
+    if (recipientSocketId) {
+      io.to(recipientSocketId).emit('privateFileResponse', {
+        from, // El remitente es quien envió el archivo
+        file,
+        fileName,
+        to, // Indicar el destinatario
+      });
+    } else {
+      console.log(`Usuario ${to} no encontrado.`);
+    }
+  
+    // Enviar el archivo de vuelta al remitente
+    const senderSocketId = Object.keys(users).find((key) => users[key] === from);
+    if (senderSocketId) {
+      io.to(senderSocketId).emit('privateFileResponse', {
+        from, // El remitente es quien envió el archivo
+        file,
+        fileName,
+        to, // Indicar el destinatario
+      });
+    }
+  });
+
+  // Abrir ventana privada de chat
+  socket.on('openPrivateChat', ({ recipient, sender }) => {
+    const recipientSocketId = Object.keys(users).find((key) => users[key] === recipient);
+    if (recipientSocketId) {
+      io.to(recipientSocketId).emit('openPrivateChat', sender);
+    }
   });
 
   // Desconexión del usuario
